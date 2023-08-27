b955e82cdf8e47d7f55077f259278e61cd4290bf
Ludovic-LABORDE <laborde.contact1@gmail.com>
2023-08-27 19:31:42 +0200

    [Project] Hotfix css + image added

--- src/components/Section/Skills/Techno.js
+++ src/components/Section/Skills/Techno.js
@@ -2,29 +2,39 @@ export const skills = [
     {
         id: 1,
         name: 'React',
-        value: '90%',
+
         image: '/src/assets/image/Logo/react-logo.png',
         size: '90'
     },
     {
         id: 2,
         name: 'PHP/Laravel',
-        value: '60%',
         image: '/src/assets/image/Logo/laravel-logo.png',
         size: '150'
     },
     {
         id: 3,
         name: 'MySQL',
-        value: '60%',
         image: '/src/assets/image/Logo/mysql-logo.png',
         size: '90'
     },
     {
         id: 4,
         name: 'Whimsical',
-        value: '80%',
         image: '/src/assets/image/Logo/whimsical-logo.png',
         size: '150'
     },
+    {
+        id: 5,
+        name: 'Sass',
+        image: '/src/assets/image/Logo/sass-logo.png',
+        size: '80'
+    },
+    {
+        id: 6,
+        name: 'Git',
+        image: '/src/assets/image/Logo/git-logo.png',
+        size: '80'
+    },
+
 ]

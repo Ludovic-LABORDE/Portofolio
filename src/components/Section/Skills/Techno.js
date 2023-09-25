5081496152a4092f84e72d12d6db51f6f71be413
Ludovic-LABORDE <laborde.contact1@gmail.com>
2023-09-25 14:21:42 +0200

    [build] hotfix images

--- src/components/Section/Skills/Techno.js
+++ src/components/Section/Skills/Techno.js
@@ -3,37 +3,37 @@ export const skills = [
         id: 1,
         name: 'React',
 
-        image: '/src/assets/image/Logo/react-logo.png',
+        image: '/static/images/Logo/react-logo.png',
         size: '90'
     },
     {
         id: 2,
         name: 'PHP/Laravel',
-        image: '/src/assets/image/Logo/laravel-logo.png',
+        image: '/static/images/Logo/laravel-logo.png',
         size: '150'
     },
     {
         id: 3,
         name: 'MySQL',
-        image: '/src/assets/image/Logo/mysql-logo.png',
+        image: '/static/images/Logo/mysql-logo.png',
         size: '90'
     },
     {
         id: 4,
         name: 'Whimsical',
-        image: '/src/assets/image/Logo/whimsical-logo.png',
+        image: '/static/images/Logo/whimsical-logo.png',
         size: '150'
     },
     {
         id: 5,
         name: 'Sass',
-        image: '/src/assets/image/Logo/sass-logo.png',
+        image: '/static/images/Logo/sass-logo.png',
         size: '80'
     },
     {
         id: 6,
         name: 'Git',
-        image: '/src/assets/image/Logo/git-logo.png',
+        image: '/static/images/Logo/git-logo.png',
         size: '80'
     },
 
